fdbdea38dcf3a602072206851fe7fc55b84bebd9
David Smith <davidsmith2@gmail.com>
2016-12-30 20:11:15 -0500

    Second attempt at #7

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ import {
 	Route,
 	Router
 } from 'react-router';
+import {useBasename} from 'history';
 
 import App from './App';
 import AppBody from './components/AppBody';
 import './index.css';
 
 ReactDOM.render(
-  <Router history={browserHistory}>
+  <Router history={useBasename(() => browserHistory)({basename: process.env.PUBLIC_URL})}>
   	<Route path="/" component={App}>
   		<IndexRoute component={AppBody} />
 		<Route path="/:mode" component={AppBody} />
